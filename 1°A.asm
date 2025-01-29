87b3746bdae8d8fb559620cfe6ee93829221a4ab
Lucas Ferruccio <lucaspaiva.ufrpe@gmail.com>
2025-01-29 00:01:12 -0300

    Ajuste nos comentários da letra a).

--- 1°A.asm
+++ 1°A.asm
@@ -1,5 +1,5 @@
 .data
-	source: .asciiz "Frase Teste"
+	source: .asciiz "Frase teste que sera copiada."
 	destination: .space 50 #Espaço separado para a copia da string, limitado a 50 caracteres
 
 .text
@@ -11,18 +11,20 @@ main:
 	
 #Função strcpy
 strcpy:
-	lb $t0, 0($a1) #Carrega o primeiro byte da source no t1
 	move $v0, $a0 #Guarda o parâmetro do reg a0 (Endereço de memoria) no reg v0
+	lb $t0, 0($a1) #Carrega o primeiro byte da source no t1
 	beq $t0, 0, end_strcpy #Caso o primeiro byte seja um caracter nulo (ou seja, string vazia) ele vai direto para o final do código
 		
 #Loop utilizado para percorrer a frase da source
 loop_strcpy:
-	sb $t0, 0($a0)
-	#Acrecimos dos Contadores
+	sb $t0, 0($a0) #Guarda no endereço de memória do destination o último caracter carregado
+	
+	#Acrecimos dos Contadores/Marcadores
 	addi $a1, $a1, 1  #Adiciona mais um no reg da source para que possamos ler o próximo caractere
 	addi $a0, $a0, 1  #Adiciona mais um no reg da destination para que possamos inserir o próximo caractere
-	lb $t0, 0($a1)
-	bne  $t0, 0, loop_strcpy #Caso o valor seja diferente do caracter nulo ('\0') ele continua a copiar o byte
+	
+	lb $t0, 0($a1) #Faz a leitura do caracter na nova posição
+	bne  $t0, 0, loop_strcpy #Caso o valor seja diferente do caracter nulo ('\0') ele continua a copiar o byte reiniciando o loop
 
 #Final do método de strcpy
 end_strcpy:
